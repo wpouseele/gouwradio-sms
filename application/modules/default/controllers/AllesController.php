02113bbacd179400d2578cfea3ebc5d0d4462cbe
wpouseele <wim.pouseele@gmail.com>
2011-10-15 10:15:36 +0200

    Set application wide locale. Upgrade to jQuery Mobile RC1. Fixed issue with special characters in json response.

--- application/modules/default/controllers/AllesController.php
+++ application/modules/default/controllers/AllesController.php
@@ -22,8 +22,21 @@ class Default_AllesController extends Zend_Controller_Action
         // disable the layout
         $this->_helper->layout->disableLayout();
         $messages = $this->_em->getRepository('Entities\Message')->getNewMessages();
-        $this->view->data = Zend_Json::encode($messages);
+		
+        $this->view->data = Zend_Json::encode($this->convertArrayKeys($messages));
     }
+	
+	private function convertArrayKeys(array $array)
+	{
+		foreach($array as $inner_key => $inner_array)
+		{
+			foreach(array_keys($inner_array) as $key){
+				$inner_array[$key] = @iconv('UTF-8', 'UTF-8//TRANSLIT', $inner_array[$key]);	
+			}
+			$array[$inner_key] = $inner_array;
+		}
+	    return $array; 
+	  } 
 
 }
 
